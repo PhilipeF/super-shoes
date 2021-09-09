14ed09008fdeeeb229af50da3673a5001b818287
Philipe Ferreira <philipesferreiraa@gmail.com>
2021-09-09 12:38:29 -0300

    carrosel react ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,64 @@
+import { useEffect, useState, useRef } from 'react';
+import '../src/App.css';
+
 function App() {
+
+  const [data, setData] = useState([]);
+  const corrossel = useRef(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/static/shoes.json')
+      .then(response => response.json())
+      .then(setData)
+  }, [])
+
+  if (!data || !data.length) return null;
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    corrossel.current.scrollLeft -= corrossel.current.offsetWidth
+
+  }
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    corrossel.current.scrollLeft += corrossel.current.offsetWidth
+
+  }
+
   return (
-    <h1>SuperShoes</h1>
+
+    <div className='container'>
+      <div className='logo'>
+        <img src='/static/images/super-shoes.png' alt='Super Shoes Logo' />
+      </div>
+      <div className='corrossel' ref={corrossel}>
+        {data.map(item => {
+          const { id, name, price, oldPrice, image } = item
+          return (
+            <div className='item' key={id}>
+              <div className='image'>
+                <img src={image} alt={item.name} />
+              </div>
+              <div className='info'>
+                <span className='name'>{name}</span>
+                <span className='oldPrice'>U$ {oldPrice}</span>
+                <span className='price'>U$ {price}</span>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className='buttons'>
+        <button onClick={handleLeftClick}>
+          <img src='/static/images/216151_right_chevron_icon.png' alt='Scroll Left' />
+        </button>
+        <button onClick={handleRightClick}>
+          <img src='/static/images/216151_right_chevron_icon.png' alt='Scroll Right' />
+        </button>
+      </div>
+
+    </div>
   );
 }
 
